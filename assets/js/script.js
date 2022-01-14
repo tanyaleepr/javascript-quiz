074bb28db77c1167a3eb0e2053c9114c78ae9f43
tanyaleepr <tanyaleepr@gmail.com>
2022-01-14 14:48:58 -0600

    variables for the entire quiz

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var quizStatus = true; // Know the status of the quiz
+var questionNumber = 0; // Track the answers
+var answerNumber = 0; // Track next answers to show
+var score = 0; // Max value by decreasing each wrong answer
+var highScore = 50; // Score add fix for ticking timer
+var finalAnswerCheck = 0 // If last answer was wrong it will be validated outside of the time interval and then display as enabled = 1 
+var checkTimes = 1 // Check timer times 
+var viewHighScoresBtnEl = document.getElementById('view-high-scores'); // View High Scores Btn El
+var startQuizBtnEl = document.getElementById('start-quiz'); 
+var answer1BtnEl = document.getElementById('answer1'); 
+var answer2BtnEl = document.getElementById('answer2'); 
+var answer3BtnEl = document.getElementById('answer3'); 
+var answer4BtnEl = document.getElementById('answer4'); 
+var submitScoreEl = document.getElementById('submitScore'); 
+var questionsEl = document.getElementById('questions'); // Questions for the page
+var pageEl = document.getElementById('page'); // page container for all elements except for header elements
+var htmlTimeLeft = document.getElementById('timeLeft'); // Display counter 
+var answerCorrectWrong = document.getElementById('answerCorrectWrong'); 
+var questionDisplayEl = document.createElement("questionDisplay"); // Display Question
+var finalScoreDisplayEl = document.createElement("finalScoreDisplay"); 
+var enterInitialsEl = document.createElement("enterInitials"); // Enter initials
+var enterInitialsTextAreaEl = document.createElement("enterInitialsTextArea"); // TextArea
+var button1234 = document.createElement("button"); // Test answer 1
+var timeLeft = 60; // asignment countdown
