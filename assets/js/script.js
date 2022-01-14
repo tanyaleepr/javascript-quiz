c406b6d8c217f3a80f9c176af6db3d541ced54bb
tanyaleepr <tanyaleepr@gmail.com>
2022-01-14 15:04:05 -0600

    variable for question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,46 @@ answer4BtnEl.style.display = 'none';
 submitScoreEl.style.display = 'none';
 answerCorrectWrong.style.display='none';
 enterInitialsTextArea.style.display='none';
+
+
+// this section is for the actual questions
+var questionsObject = { // Object that holds correct answers.
+    correct: { 
+        0 : "Commonly used datatypes DO NOT include?",
+        1 : "The condition statement if/else is enclosed with the following:",
+        2 : "Arrays can be used to store the following", // Button #4 for 
+        3 : "A very useful tool to debug arrays is:", // Button #3
+        4 : "Strings must be enclosed with:"
+    }
+};
+
+// this section is for the answes of the actual questions
+var answersObject = { // Object that holds correct answers.
+    answers: { 
+        0 : {
+            0: "Strings",
+            1: "Boolean",
+            2: "Alerts",
+            3: "Numbers"},
+        1 : {
+            0: "Parentheses",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Square Brackets"},
+        2 : { // Button #3
+            0: "Javascript",
+            1: "Terminal/bash",
+            2: "For loops", 
+            3: "Console.log"},      
+        3 : { // Answer to question 5 --> Button #2
+            0: "Commas",
+            1: "Curly brackets",
+            2: "Quotes", 
+            3: "Parentheses"},      
+        4 : { // Button #4
+            0: "Number of strings",
+            1: "Other arrays",
+            2: "Booleans",
+            3: "All of the above"},  
+    }
+};
