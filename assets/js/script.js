806e81508f59f85e8b280d1a5c85795ea52258c2
tanyaleepr <tanyaleepr@gmail.com>
2022-01-14 15:09:47 -0600

    functionality for 'view high scores'

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,29 @@ var answersObject = { // Object that holds correct answers.
             3: "All of the above"},  
     }
 };
+
+//Initialize the display timer at default value
+htmlTimeLeft.textContent = timeLeft;
+
+//this section is for View High Scores
+viewHighScoresBtnEl.addEventListener("click", function() { // View high scores
+
+    var quizUsers = "";
+    var substringTest ="";
+    var highScores = "";
+
+    for (var i=0; i < localStorage.length; i++) {
+        var checkUserValue = [];
+        
+        quizUsers = localStorage.getItem(localStorage.key(i));
+        substringTest = quizUsers.substring(0,4) 
+        if (substringTest == "quiz") {
+            checkUserValue = quizUsers.split(",");
+            var userName = checkUserValue[0]
+            highScores += "User " + userName.substring(4) + " high score is: " + checkUserValue[1] + "\n";
+       }
+    }
+    window.alert(highScores);
+
+});
+
